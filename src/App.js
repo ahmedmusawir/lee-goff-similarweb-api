972e66a16e1023868118a5abb6958854b3d7e0fd
Ahmed Musawir <moose@Ahmeds-Mac.local>
2023-02-06 22:37:54 +0800

    this will not go anywhere ...

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { useState } from 'react';
 import axios from 'axios';
 import moment from 'moment';
+import { Audio } from 'react-loader-spinner';
 // import info from './data/similarWebData';
 
 function App() {
@@ -11,6 +12,9 @@ function App() {
   const [lastDateListed, setLastDateListed] = useState('');
   const [lastVisitNumberListed, setLastVisitNumberListed] = useState('');
   const [leadNumber, setLeadNumber] = useState('');
+  const [manualVisitorNumber, setManualVisitorNumber] = useState(null);
+  const [manualLeadNumber, setManualLeadNumber] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getFormattedData = (obj) => {
     // console.log('DATA:', info[0].EstimatedMonthlyVisits);
@@ -36,8 +40,15 @@ function App() {
       .toString()
       .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     setLeadNumber(formattedLeads);
+
+    //CLEARING MANUAL VISIOR NUMBER
+    setManualVisitorNumber('');
+    //CLEARING MANUAL LEAD NUMBER
+    setManualLeadNumber(null);
   };
   const getApiData = (e) => {
+    setIsLoading(true);
+    setLeadNumber('');
     const options = {
       method: 'GET',
       url: 'https://similar-web.p.rapidapi.com/get-analysis',
@@ -53,10 +64,23 @@ function App() {
       .then(function (response) {
         const apiData = response.data.EstimatedMonthlyVisits;
         getFormattedData(apiData);
+        setIsLoading(false);
       })
       .catch(function (error) {
         console.error(error);
       });
+  };
+
+  const generateLeadNumber = (e) => {
+    const expectedManualVisitors = manualVisitorNumber
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    const manuallyGeneratedLeads = (manualVisitorNumber * percentage) / 100;
+    const formattedManualLeads = manuallyGeneratedLeads
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    setManualLeadNumber(formattedManualLeads);
+    setManualVisitorNumber(expectedManualVisitors);
   };
 
   return (
@@ -65,27 +89,81 @@ function App() {
         <h1>Lee Goff Lead Calculator</h1>
       </header>
       <main className='App-container'>
+        <h4 style={{ textAlign: 'center' }}>
+          Generate Lead number by Domain Name...
+        </h4>
         <section className='form-box'>
-          <input type='text' onChange={(e) => setDomainName(e.target.value)} />
+          <input
+            type='text'
+            onChange={(e) => setDomainName(e.target.value)}
+            placeholder='youdomain.com'
+          />
           <button onClick={getApiData}>Get Data</button>
         </section>
         <section className='display-box'>
           <div className='display-box-content'>
             <h3 style={{ color: 'darkred', textDecoration: 'underline' }}>
-              SimilarWeb Data
+              SimilarWeb Data: <br />
+              {domainName}
             </h3>
+            {isLoading && (
+              <Audio
+                height='80'
+                width='80'
+                radius='9'
+                color='dodgerblue'
+                ariaLabel='loading'
+              />
+            )}
             {lastDateListed && lastVisitNumberListed && leadNumber && (
               <div>
                 <h5>{lastDateListed}</h5>
-                <h4>{lastVisitNumberListed}</h4>
-                <p>(Expected Visitors)</p>
+                <h4>
+                  {lastVisitNumberListed}
+                  <br />
+                  <small>(Expected Visitors)</small>
+                </h4>
                 <div className='generated-leads'>
                   <h4>Lead Generated</h4>
                   {leadNumber}
                 </div>
               </div>
             )}
+            {manualVisitorNumber && manualLeadNumber && (
+              <div>
+                <h3
+                  style={{
+                    color: 'darkred',
+                    textDecoration: 'underline',
+                    marginTop: '4rem',
+                  }}
+                >
+                  Manually Generated Data:
+                </h3>
+                <h4>
+                  {manualVisitorNumber}
+                  <br />
+                  <small>(Expected Visitors)</small>
+                </h4>
+                <div className='generated-leads'>
+                  <h4>Lead Generated</h4>
+                  {manualLeadNumber}
+                </div>
+              </div>
+            )}
           </div>
+        </section>
+        <h4 style={{ textAlign: 'center' }}>
+          Generate Lead number manually...
+        </h4>
+        <section className='form-box'>
+          <input
+            type='text'
+            onChange={(e) => setManualVisitorNumber(e.target.value)}
+            placeholder='Expected Visitor Number'
+            value={manualVisitorNumber}
+          />
+          <button onClick={generateLeadNumber}>Generate Lead</button>
         </section>
       </main>
     </div>
